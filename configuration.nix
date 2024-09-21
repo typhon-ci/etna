e4ddd900f88339a00978adc15939877632d3973c
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-09-21 17:46:23 +0200

    enable acme

--- configuration.nix
+++ configuration.nix
@@ -17,5 +17,23 @@
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = import ./keys.nix;
 
+  services.nginx = {
+    enable = true;
+    virtualHosts."etna.typhon-ci.org" = {
+      addSSL = true;
+      enableACME = true;
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "typhon@typhon-ci.org";
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
   system.stateVersion = "24.05";
 }
